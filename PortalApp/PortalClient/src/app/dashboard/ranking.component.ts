f55318647504d8641cde7b14539dbcf2c9088fb2
adrian-zajac <adrian.zajac@globallogic.com>
2026-03-10 14:53:50 +0100

    SCRUM - 28

---
+++ PortalApp/PortalClient/src/app/dashboard/ranking.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-note-rating',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './note-rating.html',
+  styleUrl: './note-rating.css'
+})
+export class NoteRatingComponent {
+  helpfulness = 5;
+  easeOfCreation = 5;
+
+  emojis = ['😡', '😠', '😞', '😕', '😐', '🙂', '😊', '😄', '🤩', '🥰'];
+
+  getEmoji(rating: number): string {
+    
+    return this.emojis[rating - 1];
+  }
+}
