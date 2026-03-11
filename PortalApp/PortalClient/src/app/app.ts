c0212725f7f449788c2010780d0ad63ae6c5e968
mateusz-ryba <mateusz.ryba@globallogic.com>
2026-03-11 13:46:53 +0100

    dodanie obslugi multimediow

--- PortalApp/PortalClient/src/app/app.ts
+++ PortalApp/PortalClient/src/app/app.ts
@@ -1,10 +1,11 @@
+import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, RouterOutlet],
   templateUrl: './app.html'
 })
 export class AppComponent {}
