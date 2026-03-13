c40f844686c41cbfda7f158068c425d2201eb36c
Damian Koziarski <damian.koziarski@globallogic.com>
2026-03-13 01:01:33 +0100

    works-ranking

--- PortalApp/PortalClient/src/app/app.ts
+++ PortalApp/PortalClient/src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './login/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NoteRankingComponent } from './note-ranking/note-ranking';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DashboardComponent, RouterOutlet, NoteRankingComponent],
+  imports: [DashboardComponent, RouterOutlet], // Removed NoteRankingComponent
   templateUrl: './app.html'
 })
 export class AppComponent {
